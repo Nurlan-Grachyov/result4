ee55176fa5e4e840572426a50b267a83cd8c3e87
Nurlan <nurlan.grachyov@mail.ru>
2024-11-10 12:36:48 +0400

    Готовое 3е задание

--- src/develop.py
+++ src/develop.py
@@ -95,21 +95,34 @@ class DataFile(OperationsWithVacancies, AbstractFile):  # Remove OperationsWithV
         self.file = file
 
     def save_data(self):
-        with open(self.file, "w") as file:
-            json.dump(self.comparison_pay(), file)
+        with open(self.file, "w", encoding='utf-8') as file:
+            json.dump(self.comparison_pay(), file, ensure_ascii=False)
         return self.comparison_pay()
 
     def get_data(self):
-        # with open(self.file, "r") as file:
-        #     return json.load(file)
-        pass
+        with open(self.file, "r") as file:
+            data_from_json_file = json.load(file)
+            for vacancy in data_from_json_file:
+                if (
+                        self.name in vacancy.get("name")
+                        and self.employment in vacancy["employment"].get("name")
+                        and self.currency in vacancy["salary"].get("currency")
+                        and vacancy["salary"].get("from", 0) >= self.pay_from
+                        and vacancy["salary"].get("to", 0) <= self.pay_to
+                ):
+                    return data_from_json_file
+                else:
+                    continue
+
     def delete_data(self):
-        pass
-#         pass
+        with open(self.file, "w") as file:
+            pass
+
 if __name__ == '__main__':
     # data = GetVacancies("python")
     # sorted_data = OperationsWithVacancies("python", "Junior", "Полная занятость", "RUR", 50_000, 100_000)
     # sorted_data.filtered_vacancies()
     # print(sorted_data.lowest_pay())
     data_to_file = DataFile("python", "Junior", "Полная занятость", "RUR", 50_000, 100_000, "../data/hh_vacancies.json")
-    save_data = data_to_file.save_data()
+    save_data = data_to_file.delete_data()
+    print(save_data)
