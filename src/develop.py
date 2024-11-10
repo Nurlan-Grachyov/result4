84428fdce6fdb3dbf6fbe3b24782e190c9b25a73
Nurlan <nurlan.grachyov@mail.ru>
2024-11-10 12:24:55 +0400

    корректная работа

--- src/develop.py
+++ src/develop.py
@@ -52,7 +52,7 @@ class OperationsWithVacancies(GetVacancies):
 
     def filtered_vacancies(self):
         for vacancy in self.vacancies:
-            if vacancy.get("salary") is None or vacancy["salary"].get("to") is None:
+            if vacancy.get("salary") is None or vacancy["salary"].get("to") is None or vacancy["salary"].get("from") is None or vacancy in self.sorted_vacancies:
                 continue
             if (
                 self.name in vacancy.get("name")
@@ -61,7 +61,6 @@ class OperationsWithVacancies(GetVacancies):
                 and vacancy["salary"].get("from", 0) >= self.pay_from
                 and vacancy["salary"].get("to", 0) <= self.pay_to
             ):
-                print(vacancy)
                 self.sorted_vacancies.append(vacancy)
             else:
                 continue
@@ -69,7 +68,6 @@ class OperationsWithVacancies(GetVacancies):
         for vacancy in self.sorted_vacancies:
             avg_pay = (vacancy["salary"].get("from") + vacancy["salary"].get("to")) / 2
             vacancy["salary"]["avg"] = avg_pay
-
         return self.sorted_vacancies
 
     def comparison_pay(self):
@@ -91,25 +89,6 @@ class OperationsWithVacancies(GetVacancies):
         return min(self.sorted_vacancies, key=lambda x: x["salary"]["avg"])
 
 
-# class DataFile(AbstractFile):  # Remove OperationsWithVacancies from inheritance
-#     def __init__(self, keyword, name, employment, currency, pay_from, pay_to, file):
-#         self.operations = OperationsWithVacancies(keyword, name, employment, currency, pay_from, pay_to)
-#         self.comparison_pay = self.operations.comparison_pay()
-#         self.file = file
-#
-#     def save_data(self):
-#         with open(self.file, "w") as file:
-#             # json.dump(self.comparison_pay, file)
-#             json.dump(self.operations.sorted_vacancies, file)
-#         return self.operations.sorted_vacancies
-#
-#     def get_data(self):
-#         # with open(self.file, "r") as file:
-#         #     return json.load(file)
-#         pass
-#     def delete_data(self):
-#         pass
-
 class DataFile(OperationsWithVacancies, AbstractFile):  # Remove OperationsWithVacancies from inheritance
     def __init__(self, keyword, name, employment, currency, pay_from, pay_to, file):
         super().__init__(keyword, name, employment, currency, pay_from, pay_to)
@@ -118,7 +97,6 @@ class DataFile(OperationsWithVacancies, AbstractFile):  # Remove OperationsWithV
     def save_data(self):
         with open(self.file, "w") as file:
             json.dump(self.comparison_pay(), file)
-            print(self.comparison_pay())
         return self.comparison_pay()
 
     def get_data(self):
@@ -127,10 +105,11 @@ class DataFile(OperationsWithVacancies, AbstractFile):  # Remove OperationsWithV
         pass
     def delete_data(self):
         pass
+#         pass
 if __name__ == '__main__':
     # data = GetVacancies("python")
-    # # sorted_data = OperationsWithVacancies("python", "Junior", "Полная занятость", "RUR", 50_000, 100_000)
-    # print(sorted_data.filtered_vacancies())
+    # sorted_data = OperationsWithVacancies("python", "Junior", "Полная занятость", "RUR", 50_000, 100_000)
+    # sorted_data.filtered_vacancies()
     # print(sorted_data.lowest_pay())
     data_to_file = DataFile("python", "Junior", "Полная занятость", "RUR", 50_000, 100_000, "../data/hh_vacancies.json")
     save_data = data_to_file.save_data()
